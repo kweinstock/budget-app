d3913c991549eacdd12c19e058ecbaa7b5158a64
kweinstock <101689254+kweinstock@users.noreply.github.com>
2026-09-22 12:16:32 -0500

    cloudflare update

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   base: '/budget-app/',
+  build: {
+    outDir: "dist/budget-app",
+    emptyOutDir: true,
+  },
   plugins: [
     react(),
     VitePWA({
